609385faae13151409abf678556a09ad9595b73d
lucabaroni <loocabaroni@gmail.com>
2024-08-08 17:42:25 +0200

    update setup

--- setup.py
+++ setup.py
@@ -12,6 +12,22 @@ setup(
     packages=find_packages(exclude=[]),
     install_requires=[
         "deeplake",
+        'torch==1.13.1',
+        'torchvision==0.14.1',
+        'torchaudio==0.13.1',
+        'transformers==4.35.2',
+        'deeplake[enterprise]',
+        'wandb',
+        'moviepy',
+        'imageio',
+        'tqdm',
+        'statsmodels',
+        'param==1.5.1',
+    ],
+    dependency_links=[
+        'https://github.com/sinzlab/nnvision.git#egg=nnvision',
+        'https://github.com/MathysHub/featurevis.git#egg=featurevis',
+        'https://github.com/CSNG-MFF/imagen.git#egg=imagen',
     ],
     url="https://github.com/lucabaroni/classical_exps.git",
 )
